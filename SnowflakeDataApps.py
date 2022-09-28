10f6cd9ca54ded95f1542776065ec81602e4f02c
JayavarshiniJJ <114481850+JayavarshiniJJ@users.noreply.github.com>
2022-09-28 11:04:55 +0530

    Update SnowflakeDataApps.py

--- SnowflakeDataApps.py
+++ SnowflakeDataApps.py
@@ -173,10 +173,11 @@ with tab1:
         mcol = st.selectbox('Select Column:',list(set(final2.loc[final2['TABLE NAME']==mtable]['COLUMN NAME'])),key=9)
         final3 = final2.loc[final2['TABLE NAME']==mtable]
         final4dt = final3.loc[final3['COLUMN NAME']==mcol]['DATA TYPE']
-        cur.execute("Use database {};".format(DB))
-        cur.execute("show masking policies in SCHEMA {};".format(mschema))
-        schemapolicies = pd.read_sql("select * from table(result_scan(last_query_id(1)));",conn)
-        schemapolicies
+        if st.checkbox("See Masks"):
+          cur.execute("Use database {};".format(DB))
+          cur.execute("show masking policies in SCHEMA {};".format(mschema))
+          schemapolicies = pd.read_sql("select * from table(result_scan(last_query_id(1)));",conn)
+          schemapolicies
         #name = st.text_input('Name of the mask:')
         #roles_acc = pd.read_sql("select name from SNOWFLAKE.ACCOUNT_USAGE.ROLES where deleted_on is null;",conn)
        # R = []
