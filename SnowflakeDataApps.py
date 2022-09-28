d2e4fd3375c8a6358107a260021df2f5d865e506
JayavarshiniJJ <114481850+JayavarshiniJJ@users.noreply.github.com>
2022-09-28 13:39:31 +0530

    Update SnowflakeDataApps.py

--- SnowflakeDataApps.py
+++ SnowflakeDataApps.py
@@ -179,7 +179,7 @@ with tab1:
         schemapolicies = pd.read_sql("select * from table(result_scan(last_query_id()));",conn)
         applypolicy = st.selectbox('Select policy:',list(schemapolicies['name']))
         policyinfo = pd.read_sql("select POLICY_SIGNATURE from SNOWFLAKE.ACCOUNT_USAGE.MASKING_POLICIES where deleted is null and POLICY_CATALOG = '{}' and policy_schema = '{}';".format(DB,mschema),conn)
-        for x in policyinfo[['POLICY_SIGNATURE']]:
+        for x in policyinfo[['POLICY_SIGNATURE']].to_dict():
           print(x)
           #for y in x['arguments']:
            # z =  y['datatype']
